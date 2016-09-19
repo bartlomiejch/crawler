c6a709706cb62ad5d16198371795d2c895b0f9de
bartek <bartek.chowaniec@gmail.com>
2016-09-19 23:27:16 +0200

    add bs4

--- crowler.py
+++ crowler.py
@@ -1,1 +1,10 @@
 import urllib2
+from bs4 import BeautifulSoup
+
+url = 'http://www.polityka.pl/forum/'
+
+page = urllib2.urlopen(url)
+content = page.read()
+soup = BeautifulSoup(content)
+
+print soup.find_all('li')
